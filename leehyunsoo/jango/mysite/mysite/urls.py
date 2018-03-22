1d34e6d1abdacf58210dbeb4f673109c9e3743d0
leehyunsoo <lhs950204@naver.com>
2018-03-22 15:51:44 +0900

    5장 진행중

--- leehyunsoo/jango/mysite/mysite/urls.py
+++ leehyunsoo/jango/mysite/mysite/urls.py
@@ -15,13 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from polls import views
 
+# app_name ='polls'
 urlpatterns = [
-    path('polls/', views.index, name='index'),  # /polls/
-    path('polls/<int:question_id>/', views.detail, name='detail'),  # /polls/5/
-    path('polls/<int:question_id>/results/', views.results, name='results'),  # /polls/5/results/
-    path('polls/<int:question_id>/vote/', views.vote, name='vote'),  # /polls/5/vote/
+
+    # path('',),
+    path('polls/', include('polls.urls',namespace= 'polls')),  # /polls/
+    path('books/', include('books.urls',namespace= 'books')),  # /polls/
     path('admin/', admin.site.urls),
 
 ]
