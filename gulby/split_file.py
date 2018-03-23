e571bcd2c467a440acff0f7a600bb58615225bac
gulby <gulby@maukistudio.com>
2018-03-23 14:16:38 +0900

    파일 쪼개는 프로그램 간단히 작성

---
+++ gulby/split_file.py
@@ -0,0 +1,19 @@
+import sys
+import math
+import os
+
+original_file_name = sys.argv[1]
+split_count = int(sys.argv[2])
+
+with open(original_file_name, 'r') as original_file:
+    lines = list(original_file.readlines())
+
+split_size = int(math.ceil(len(lines) / split_count))
+split_lines = (lines[i:i+split_size] for i in range(0, len(lines), split_size))
+for i, splits in enumerate(split_lines):
+    split_file_name = '{}.{}'.format(original_file_name, i)
+    with open(split_file_name, 'w') as split_file:
+        split_file.writelines(splits)
+
+os.remove(original_file_name)
+print('complete')
