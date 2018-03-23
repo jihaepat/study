bc5b20eb8c6cb2fe840ed471862aa67dd92a029e
leehyunsoo <lhs950204@naver.com>
2018-03-23 18:40:08 +0900

    TDD

--- leehyunsoo/TDD/superlists/functional_test.py
+++ leehyunsoo/TDD/superlists/functional_test.py
@@ -1,18 +1,58 @@
+import time
 from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+# from selenium.webdriver.common.keys import keys
 import unittest
+from time import sleep
+
+from selenium.webdriver.common.keys import Keys
+
+
 
 class NewVisitorTest(unittest.TestCase):
 
+
+
     def setUp(self):
-        self.browser = webdriver.Chrome('/home/leehyunsoo/work/TDD/study/leehyunsoo/TDD/chromedriver')
-        self.browser.implicitly_wait(100)
+        # self.browser = webdriver.Chrome('/home/leehyunsoo/work/TDD/study/leehyunsoo/TDD/chromedriver')
+        self.browser = webdriver.Firefox(executable_path = '/home/leehyunsoo/work/TDD/study/leehyunsoo/TDD/geckodriver')
+
+        # self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
+
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        table = self.browser.find_element_by_id('id_list_table')
+
+        inputbox.send_keys('공작깃털 사기')
+
+        # sleep(5)
+        inputbox.send_keys(Keys.ENTER)
+
+        rows = table.find_elements_by_tag_name('td')
+
+        self.assertTrue(
+            any(row.text == '1: 공작깃털 사기' for row in rows),
+            '신규 작업이 테이블에 표시되지 않는다 -- 해당 텍스트:\n%s' %(
+                '123123'
+            )
+        )
+
+        # self.assertIn('1: 공장깃털 사기', [row.text for row in rows])
+
         self.fail('Finish the test!')
 
 if __name__ == '__main__':
--- leehyunsoo/TDD/superlists/lists/tests.py
+++ leehyunsoo/TDD/superlists/lists/tests.py
@@ -1,16 +1,53 @@
+from django.http import HttpRequest
+from django.shortcuts import render
+from django.template.loader import render_to_string
 from django.urls import resolve
 from django.test import TestCase
 from .views import home_page
+import re
 
 # Create your tests here.
 #
 # class SmokeTest(TestCase):
-#
 #     def test_bad_maths(self):
 #         self.assertEqual(1 + 1, 3)
 
 class HomePageTest(TestCase):
 
+    pattern_input_csrf = re.compile(r'<input[^>]*csrfmiddlewaretoken[^>]*>')
+
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        # self.assertTrue(response.content.startswith(b'<html>'))
+        # self.assertIn(b'<title>To-Do lists</title>', response.content)
+        # self.assertTrue(response.content.endswith(b'</html>'))
+
+        expected_html = render_to_string('home.html')
+
+        self.assertEqual(
+            re.sub(self.pattern_input_csrf, '', response.content.decode()),
+            re.sub(self.pattern_input_csrf, '', expected_html),
+        )
+
+    def test_home_page_can_save_a_POST_request(self):
+
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = '신규 작업 아이템'
+
+        response = home_page(request)
+
+        self.assertIn('신규 작업 아이템', response.content.decode())
+        expected_html = render_to_string(
+            'home.html',
+            {'new_item_text': '신규 작업 아이템'},
+        )
+        self.assertEqual(
+            re.sub(self.pattern_input_csrf, '', response.content.decode()),
+            re.sub(self.pattern_input_csrf, '', expected_html),
+        )
