9aa8f7d051baf91babf64c5de4a2490383072044
leehyunsoo <lhs950204@naver.com>
2018-04-12 16:58:26 +0900

    deployment.sh 수정

--- leehyunsoo/deployment.sh
+++ leehyunsoo/deployment.sh
@@ -9,5 +9,8 @@ git clone https://github.com/jihaepat/fastText.git
 # word-embeddings-benchmarks 다운
 git clone https://github.com/jihaepat/word-embeddings-benchmarks.git
 
+cd fastText
 pip install -U -r fastText/.
+cd ..
+cd word-embeddings-benchmarks
 pip install -U -r word-embeddings-benchmarks/requirements.txt
