cde3542e1cde996db2386a320fec630582abb267
YoonJaeLee <asas1994@naver.com>
2018-04-13 20:34:12 +0900

    wordnet

---
+++ yoon/wordnet/save_data.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+python meronyms_wordnet.py > /home/yoonjae/문서/test/ttt.txt
--- yoon/wordnet/wordnet.py
+++ yoon/wordnet/wordnet.py
@@ -1,33 +1,67 @@
-import sys
-from termcolor import colored, cprint
 from nltk.corpus import wordnet
+from nltk.corpus import wordnet as wn
+def func_synsets():
+    wn.synsets('dog')
+    wn.synsets('dog', pos=wn.VERB)
+    wn.synset('dog.n.01').definition()
+    ret_synsets = wn.synset('dog.n.01')
+    return ret_synsets
 
-text = colored('========================', 'green')
-text_pint = colored(':', 'red',attrs=['blink','bold'])
-print(text)
-print(wordnet.synsets('lunch'))
+def func_similarity():
+    dog = wn.synset('dog.n.01')
+    cat = wn.synset('cat.n.01')
+    ret_similar = dog.fun_similarity(cat)
+    return ret_similar
 
-lunch_n = wordnet.synsets('lunch')[0]
-print(lunch_n)
+def func_morpy():
+    ret_morpy = wn.morphy('denied', wn.VERB)
+    return ret_morpy
 
-print('lunch_n.examples 예시', text_pint, lunch_n.examples())
+def func_synset_closures():
+    dog = wn.synset('dog.n.01')
+    hypo = lambda s: s.hyponyms()
+    ret_closures = list(dog.closure(hypo, depth=1)) == dog.hyponyms()
+    return ret_closures
 
-print('lunch_n.part_meronyms 부분어', text_pint, lunch_n.part_meronyms())
+# print(func_synsets())
+# print(func_similarity())
+# print(func_morpy())
+# print(func_synset_closures())
 
-lunch_v1 = wordnet.synsets('lunch')[1]
-print('wordnet.synsets(lunch)[1]', text_pint, lunch_v1)
 
-print(dir(lunch_n))
 
-print('lunch_n.hyponyms 하의어', text_pint, lunch_n.hyponyms())
+"""
+유의어(synsets)            : 비슷한 뜻을 가진 단어 출력
+보조어(lemmas)             : 단어의 부명제
+상위어(hypernyms)          : X가 Y의 한 종류이면 Y는 X의 상위어이다
+하위어(hyponyms)           : Y가 X의 한 종류이면 Y는 X의 하위어이다
+전체어(holonyms)           : X가 Y의 부분이면 Y는 X의 전체어이다
+(verb_frame)              :
+유사도(similarity)         : 단어간 비슷한 정도
+(Access_to_all_Synsets)   : 해당 식에 맞는 단어 검색
+(Morphy)                  : 단어의 형태소를 변환
 
-print('lunch_n.hypernyms 상의어', text_pint,  lunch_n.hypernyms())
 
-print(lunch_n.hypernyms()[0].hyponyms())
+동사의 분사(participle of verb)
+파생된 형용사(root adjectives)
+ : 단어에 대한 설명을 출력
+synsets.lemmas : 단어의 부명제
+synsets.hypernyms : 상위어
+.member_holonyms : 동의어
+.root_hypernyms : 최상단 상위어
+.lowest_common_hypernyms() : 가장 낮은 상위 등위어
+Similarity :
+fun_similarity : 두 단어간 유사도
+Access to all Synsets : 모든 단어를 검색
+Morphy : 단어가 어떤 형태인지
+wn.morphy('denied', wn.VERB)
+Synset Closures : 단어와 단어사이 유사도
 
-for s in wordnet.synsets('eye'):
-    print(s, s.definition())
+"""
 
-eye = wordnet.synsets('eye')[0]
-print('eye.part_meronyms 부분어', text_pint, eye.part_meronyms())
-print(text)
+
+
+
+
+
+
