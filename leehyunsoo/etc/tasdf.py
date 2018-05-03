9496cf73562b65f5484226806796fe44a73f954c
leehyunsoo <lhs950204@naver.com>
2018-05-03 15:38:57 +0900

    baidu 검색 필터 적용 but 조건과 맞지 않는 데이터가 보여지는 경우가 다수

--- leehyunsoo/etc/tasdf.py
+++ leehyunsoo/etc/tasdf.py
@@ -1,9 +1,12 @@
-file = open('test.json','r')
-ttt = file.readlines()
-print(ttt[-2])
-file2 = open('test.json','r+')
-tttw = file2.readlines()
-print(tttw[-2])
-file2.write('111')
-# file.writelines(-2,'ttt')
-# print(file.)
+ar = ['1','2','3','1']
+ttt={'a':'1'}
+ar2= []
+
+url = 'https://www.baidu.com/s?wd=asic&pn=100&gpc=stf%3D1525186800%2C1525359598%7Cstftype%3D2'
+url2 = 'https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=1&tn=baidu&wd=asic&oq=asic&rsv_pq=b31fc32f00035ad6&rsv_t=477bEh8oeb9TKg8%2FFid7cFM75fa%2B5ikRnrSxOwduNo7YzI3myW4BHlvE0kM&rqlang=cn&rsv_enter=1&gpc=stf%3D1525100400%2C1525359598%7Cstftype%3D2&tfflag=1&rsv_srlang=cn&sl_lang=cn&rsv_rq=cn'
+ttt = url2.split('&')
+
+print(ttt)
+for x in ttt:
+    if 'gpc' in x:
+        print(x)
