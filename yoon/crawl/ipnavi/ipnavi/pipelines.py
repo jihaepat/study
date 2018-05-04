9238496696b228e08d21de15e5253c272509e8a4
YoonJaeLee <asas1994@naver.com>
2018-05-04 18:58:06 +0900

    자동으로 최신 버전이 저장되도록 datetime 사용

--- yoon/crawl/ipnavi/ipnavi/pipelines.py
+++ yoon/crawl/ipnavi/ipnavi/pipelines.py
@@ -5,18 +5,25 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 import json
-from scrapy.exporters import JsonItemExporter
+import datetime
+import operator
 
 class IpnaviPipeline(object):
+    prnt_date = datetime.datetime.now().strftime('%Y%m%d%H%M')
     def open_spider(self, spider):
         print('start_crawl')
-        self.file = open('ipnavi.json', 'w', encoding='utf-8')
+        print(self.prnt_date)
+        self.file = open('/media/yoonjae/4TB2/ipnavi/ipnavi_'+self.prnt_date+'.json', 'w+', encoding='utf-8')
         # self.file.write('[\n')
 
     def close_spider(self, spider):
         # self.file.write(']\n')
-        self.file.close()
         print('enc_crawl')
+
+    def sort(self):
+        sorted(self.file, key='no.get')
+        print(self.file.read())
+        self.file.close()
 
     def process_item(self, item, spider):
         line = json.dumps(dict(item), ensure_ascii=False) + ',\n'
